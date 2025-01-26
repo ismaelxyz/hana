58a761405477392081b7a879dbe426369235c9fc
ismaelxyz <50039280+ismaelxyz@users.noreply.github.com>
2025-01-26 23:56:01 +0000

    feat: added env var CARGO_RUN_BUILD

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
-use std::fs::File;
-use std::io::{self, Write};
-use std::process::Command;
+use std::{
+    env,
+    fs::File,
+    io::{self, Write},
+    process::Command,
+};
 
 #[cfg(target_os = "linux")]
 const OS: &str = "Linux";
@@ -12,24 +15,24 @@ const OS: &str = "MacOs";
 const OS: &str = "Windows";
 
 fn main() {
-    let output = Command::new("rustc")
-        .arg("-Vv")
-        .output()
-        .expect("Rustc command failed to start");
+    if env::var("CARGO_RUN_BUILD").is_ok() {
+        let output = Command::new("rustc")
+            .arg("-Vv")
+            .output()
+            .expect("Rustc command failed to start");
 
-    io::stderr().write_all(&output.stderr).unwrap();
-    assert!(output.status.success());
+        io::stderr().write_all(&output.stderr).unwrap();
+        assert!(output.status.success());
 
-    let source = String::from_utf8(output.stdout).unwrap();
-    let rustc = &source.split('\n').collect::<Vec<&str>>();
-    let release = &rustc[rustc.len() - 3][9..];
+        let source = String::from_utf8(output.stdout).unwrap();
+        let rustc = &source.split('\n').collect::<Vec<&str>>();
+        let release = &rustc[rustc.len() - 3][9..];
 
-    let ver = format!(
-        "pub const VERSION: &str = \"Haru {}\\n({}) on {}\";",
-        release,
-        OS,
-        &rustc[0]
-    );
-    let mut file = File::create("src/consts.rs").unwrap();
-    file.write_all(ver.as_bytes()).unwrap();
+        let ver = format!(
+            "pub const VERSION: &str = \"Haru {}\\n({}) on {}\";",
+            release, OS, &rustc[0]
+        );
+        let mut file = File::create("src/consts.rs").unwrap();
+        file.write_all(ver.as_bytes()).unwrap();
+    }
 }
